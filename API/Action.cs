af1dcba5c26a91cc7e761c0ac5a020a01184e52f
Cristian Cioanca <ccioanca@users.noreply.github.com>
2024-05-24 20:04:46 -0400

    Configured C# + Tank Graphics

---
+++ API/Action.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Godot;
+
+namespace Tankathon.API;
+
+public class Action
+{
+	public void Scan()
+	{
+	}
+
+	public void MoveForward() { }
+
+	public void Aim(Vector2 aim) { }
+
+	public void Fire() { }
+}
---
+++ MyTank/MyTank.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using System;
+using Tankathon.API;
+
+public class MyTank : ITank 
+{
+    public string _name;
+    public string Name { get => _name; set => _name = value; }
+
+    public void Do()
+    {
+        
+    }
+}
